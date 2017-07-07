aab2be500ff5b026f3bfe4cd2fc7dd7130a0a587
Johannes Villmow <johannes.villmow@student.hs-rm.de>
2017-07-07 11:39:41 +0200

    modified install script

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+BASE_DIR="$( pwd )"
+
 # get newest version of pathogen (vim packet manager)
-curl -LSso ./.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+# curl -LSso ${BASE_DIR}/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 #mv .vimrc ~/.vim/vimrc
-ln -s ./.vim/ ~/.vim
-ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ${BASE_DIR}/.vim ~/.vim
+ln -s ${BASE_DIR}/.vim/vimrc ~/.vimrc
